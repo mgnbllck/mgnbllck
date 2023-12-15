ac7bd33204ec4a8832fdb1a470fde7792bd37893
mgnbllck <151893339+mgnbllck@users.noreply.github.com>
2023-12-14 21:50:33 -0500

    magic 8 ball

---
+++ magic8ball.js
@@ -0,0 +1,46 @@
+let userName = '';
+if (userName === 'Megan'){
+  console.log('Hello, Megan!');
+} else {
+  console.log('Hello!')
+}
+
+let userQuestion = 'What is for dinner?';
+ console.log(userQuestion);
+
+ let randomNumber = Math.floor(Math.random() * 8);
+
+ let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+  eightBall = "try again";
+  break;
+  case 1:
+  eightBall = "roast";
+  break;
+  case 2:
+  eightBall = "chicken taco thing";
+  break;
+  case 3:
+  eightBall = "roast";
+  break;
+  case 4:
+  eightBall = "Chicken taco thing";
+  break;
+  case 5:
+  eightBall = "Roast";
+  break;
+  case 6:
+  eightBall = "chicken taco thing";
+  break;
+  case 7: 
+  eightBall = "roast";
+  break;
+  case 8:
+  eightball = "chicken taco thing";
+  break;
+}
+
+console.log(eightBall);
+
